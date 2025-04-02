6cf21793e4f19012482418f75b78b269a0c09511
JoandersonReis <joandersonreis470@gmail.com>
2025-04-02 09:48:37 -0300

    Change description

--- web/src/app/painel/page.tsx
+++ web/src/app/painel/page.tsx
@@ -4,12 +4,15 @@ export default function Home() {
   return (
     <div className="h-full flex items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-10">
-        <h1 className="text-4xl">Bem vindo ao TradeSee Todo</h1>
+        <h1 className="text-4xl">Bem vindo ao To-do</h1>
         <p className="text-center opacity-70 max-w-[600px]">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
-          tempore neque, iusto, iste nesciunt autem quae maxime deleniti id
-          adipisci saepe vel, rerum dolorum ducimus explicabo commodi aspernatur
-          aut voluptatibus.
+          Aqui, você pode adicionar, organizar e gerenciar suas tarefas de
+          maneira simples e intuitiva. Seja para compromissos diários, prazos de
+          trabalho ou metas pessoais, nossa plataforma está pronta para ajudar
+          você a se manter no controle e alcançar seus objetivos. Explore as
+          funcionalidades, crie suas listas e comece a transformar sua rotina
+          com um clique. Estamos aqui para ajudar você a ser mais produtivo,
+          organizado e focado no que realmente importa.
         </p>
 
         <NewDocumentModal />
